fea3c4f22a974b8e2aee6e83a2632e8e91cbd29e
Kostas Thermos <konstantinos.thermos@persgroep.nl>
2016-11-10 16:10:37 +0100

    Fix dashboard title

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from "ng2-translate";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private titleService: Title, private translateService: TranslateService) {
+  }
+
+  ngOnInit() {
+    this.translateService.get('titles.dashboard').subscribe((res: string) => {
+      this.titleService.setTitle(res);
+    });
+  }
 }
